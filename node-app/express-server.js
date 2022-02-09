3842132562226f3d4058793135e2085ae434e82a
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-02-09 16:52:28 +0530

    express server

--- node-app/express-server.js
+++ node-app/express-server.js
@@ -1,3 +1,15 @@
 
 // Step 1: Import express
 const express = require("express");
+
+// Step 2: Create Server
+const server = express();
+
+// Step 3: Listen to clients
+server.listen(3200);
+
+// Step 4: Take requests
+
+server.get("/", (req, res)=>{
+    res.end("Hello, You are connected to express server");
+});
