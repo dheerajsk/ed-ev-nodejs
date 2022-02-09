d05fbaabe71efd7c0fb48cf196a364c4db42b9c6
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-02-09 16:27:37 +0530

    node server

---
+++ node-app/node-server.js
@@ -0,0 +1,16 @@
+
+// Step 1: Import http module.
+
+const http = require("http");
+// http is part of node core modules.
+
+// Step 2: Create http server using nodejs.
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello, you are connected to nodejs server");
+});
+
+// Step 3: Listening for requests from client.
+server.listen(3200);
+
+console.log("Server is listening on 3200");
