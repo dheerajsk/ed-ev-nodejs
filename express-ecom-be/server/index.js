ecfab2fa5947409f5a5cd33572c20af2a9fa8675
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-02-09 17:12:32 +0530

    express-ecom-be

---
+++ express-ecom-be/server/index.js
@@ -0,0 +1,17 @@
+
+// Step 1: Import express
+const express = require("express");
+
+// Step 2: Create Server
+const server = express();
+
+// Step 3: Listen to clients
+server.listen(3200);
+
+// Step 4: Take requests
+
+server.get("/", (req, res)=>{
+    res.end("Hello, You are connected to express server");
+});
+
+console.log("Server is listening on 3200...");
