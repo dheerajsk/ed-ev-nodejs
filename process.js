52317fe07d95390141bc298afb2a7d2e1ca39139
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-02-18 17:25:21 +0530

    Callback within process

--- process.js
+++ process.js
@@ -2,6 +2,10 @@ const process = require("child_process");
 
 // process.spawn("node task.js");
 
-const child_Process = process.exec("nohup node task.js >/dev/null 2>&1 &")
+const child_Process = process.exec("nohup node tas.js >/dev/null 2>&1 &", (err, out, stderr)=>{
+    console.log(err);
+    console.log(stderr);
+    console.log(out)
+})
 console.log(child_Process.pid);
 
